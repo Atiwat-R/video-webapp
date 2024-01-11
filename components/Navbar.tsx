ca3a4be95045e8b398276183f05368af6ace182b
Atiwat-R <75544677+Atiwat-R@users.noreply.github.com>
2024-01-11 17:43:16 +0700

    Updated Navbar
    - UI component Navbar updated

---
+++ components/Navbar.tsx
@@ -0,0 +1,91 @@
+import { useCallback, useEffect, useState } from "react"
+import { BsChevronDown, BsSearch, BsBell } from "react-icons/bs"
+import MobileMenu from "./MobileMenu"
+import NavbarItem from "./NavbarItem"
+import AccountMenu from "./AccountMenu"
+
+const TOP_OFFSET = 66
+const NAVBAR_SHOW_TIMEOUT = 5000 // miliseconds (1000 ms = 1 sec)
+
+const Navbar = () => {
+
+    const [showMobileMenu, setShowMobileMenu] = useState(false)
+    const [showAccountMenu, setShowAccountMenu] = useState(false)
+    const [showBackground, setShowBackground] = useState(false)
+
+    const toggleMobileMenu = useCallback(() => {
+        setShowMobileMenu((current) => !current)
+    }, [])
+    const toggleShowAccountMenu = useCallback(() => {
+        setShowAccountMenu((current) => !current)
+    }, [])
+
+    useEffect(() => {
+        const handleScroll = () => {
+
+            if (window.scrollY >= TOP_OFFSET) {
+                setShowBackground(true)
+
+                // No longer show background after NAVBAR_SHOW_TIMEOUT
+                setTimeout(() => {
+                    setShowBackground(false);
+                }, NAVBAR_SHOW_TIMEOUT);
+            } else {
+                setShowBackground(false)
+            }
+        }
+        window.addEventListener('scroll', handleScroll)
+        return () => window.removeEventListener('scroll', handleScroll)
+    }, [])
+
+
+    return (
+        <nav className='w-full fixed z-40'>
+            <div className={`px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 ${showBackground ? "bg-zinc-900 bg-opacity-90" : ""}`}>
+                <div className='flex-row ml-8 gap-7 hidden lg:flex'>
+                    <NavbarItem label='Home' />
+                    <NavbarItem label='About' />
+                    <NavbarItem label='Profile' />
+                    <NavbarItem label='Option' />
+                </div>
+                <div onClick={toggleMobileMenu} className='lg:hidden flex-row items-center gap-2 ml-8 cursor-pointer relative'>
+                    <p className='text-white text-sm'>Browse </p>
+                    <BsChevronDown className={`w-4 text-white fill-white transition ${showMobileMenu ? 'rotate-180' : 'rotate-0'}`} /> {/** Use backticks ` instead of quotation marks ' or " */}
+                    <MobileMenu visible={showMobileMenu}/>
+                </div>
+                <div onClick={toggleShowAccountMenu} className='flex flex-row ml-auto gap-7 items-center '>
+                    <div className='text-gray-200 hover:text-gray-300 cursor-pointer transition'>
+                        <BsSearch />
+                    </div>
+                    <div className='text-gray-200 hover:text-gray-300 cursor-pointer transition'>
+                        <BsBell />
+                    </div>
+                    <div className='flex flex-row items-center gap-2 cursor-pointer relative'>
+                        <div className='w-6 h-6 lg:w-10 lg:h-10 rounded-md overflow-hidden'>
+                            <img src='/images/default-green.png' alt=''/>
+                        </div> 
+                        <BsChevronDown className={`text-white transition ${showAccountMenu ? 'rotate-180' : 'rotate-0'}`} /> {/** Use backticks ` instead of quotation marks ' or " */}
+                        <AccountMenu visible={showAccountMenu}/>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+
+
+
+export default Navbar
+
+/*
+
+
+<div className=''>
+
+</div> 
+
+
+
+
+*/
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
+import Navbar from "@/components/Navbar"
 import useCurrentUser from "@/hooks/useCurrentUser"
 import { NextPageContext } from "next"
-import { signOut, getSession } from "next-auth/react"
+import { getSession } from "next-auth/react"
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context)
@@ -26,8 +27,41 @@ export default function Home() {
 
   return (
     <>
-      <h1 className='text-7xl text-neutral-300' >Welcome { user?.name }</h1>
-      <button onClick={() => signOut()} className='h-10 w-full bg-white'>Logout</button>
+      <Navbar />
+      
+      <div className='bg-gray-500' >
+        <div className='h-96' ></div>
+        <div className='h-96' ></div>
+        <div className='h-96' ></div>
+        <div className='h-96' ></div>
+        <div className='h-96' ></div>
+        <div className='h-96' ></div>
+        <div className='h-96' ></div>
+        <div className='h-96' ></div>
+        <div className='h-96' ></div>
+        <div className='h-96' ></div>
+        <div className='h-96' ></div>
+        <div className='h-96' ></div>
+        <div className='h-96' ></div>
+        <div className='h-96' ></div>
+        <div className='h-96' ></div>
+        <div className='h-96' ></div>
+        <div className='h-96' ></div>
+        <div className='h-96' ></div>
+        <div className='h-96' ></div>
+        <div className='h-96' ></div>
+        <div className='h-96' ></div>
+        <div className='h-96' ></div>
+        <div className='h-96' ></div>
+        <div className='h-96' ></div>
+        <div className='h-96' ></div>
+        <div className='h-96' ></div>
+        <div className='h-96' ></div>
+        
+      </div>
+
+      
+      
     </>
   )
 }
